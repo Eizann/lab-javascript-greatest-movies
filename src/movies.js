910cc1a3fe5711f00277a35cef6a174da4933787
Tom <eizandev@gmail.com>
2021-12-05 23:57:55 +0100

    small modifications

--- src/movies.js
+++ src/movies.js
@@ -14,21 +14,28 @@ function howManyMovies(arr) {
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
 function scoresAverage(arr) {
+
   function roundToTwo(num) {
     return +(Math.round(num + "e+2") + "e-2");
   }
 
-  if (arr.length === 0) {
-    return 0;
-  }
+  if (arr.length === 0) return 0;
+  
   const average = arr.reduce(function (sum, movie) {
     return sum + movie.score;
   }, 0);
+
+/*   const numberOfScores = arr.reduce(function (accumulator, movie) {
+    if (movie.score) {
+      accumulator += movie.score;
+    }}, 0);
+ */
   return roundToTwo(average / arr.length);
 }
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore(arr) {
+
   function roundToTwo(num) {
     return +(Math.round(num + "e+2") + "e-2");
   }
@@ -38,7 +45,7 @@ function dramaMoviesScore(arr) {
       return sum + movie.score;
     }
   }, 0);
- 
+
   return roundToTwo(average / arr.length);
 }
 
@@ -53,26 +60,49 @@ function orderByYear(arr) {
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 function orderAlphabetically(arr) {
   let sortedTitle = [...arr].sort((a, b) => {
-    a.title - b.title;
+    if (a.title < b.title) {
+      return -1;
+    }
+    if (a.title < b.title) {
+      return -1;
+    }
+    return 0;
   });
 
   sortedTitle = sortedTitle.map(movies => movies.title);
-  // console.log(sortedTitle);
-  // let firstTitles = [];
-  // for (let i = 0; i < sortedTitle.length; i++) {
-  //   firstTitles.push(sortedTitle.title);
-  //   if (i === 19) {
-  //     break;
-  //   }
-  // }
+
   return sortedTitle.slice(0, 20);
  }
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
-function turnHoursToMinutes() { }
+function turnHoursToMinutes(arr) {
+/*   arr.map(function (movie) {
+    let newArr = [...arr];
+    
+    for (let i = 0; i < newArr.length; i++) {
+      let tmp = [];
+      let hours = 0;
+      let mins = 0;
+      tmp = newArr.duration.split(' ');
+      console.log(tmp);
+    }
+  })
+  return newArr; */
+ }
 
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
-function bestYearAvg() { }
+function bestYearAvg(arr) { 
+  if (arr.length === 0) return null;
+  let bestYear = 0;
+  let bestScore = 0
+  arr.reduce(function(bestScore, movie) {
+    if (movie.score > bestScore) {
+      bestYear = movie.year;
+      bestScore = movie.score;
+    }
+  }, 0)
+  return `The best year was ${bestYear} with an average score of ${bestScore}`;
+}
 
 
 
